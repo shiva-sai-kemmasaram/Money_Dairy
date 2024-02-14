e3b8bc411f3b357bef7305828e264942436c5878
shiva-sai-kemmasaram <201216@iiitt.ac.in>
2024-02-14 23:44:45 +0530

    new

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 var fs = require('fs');
 require("./db/conn");
 require('dotenv').config()
+
 const app = express()
 const diary = require('./routes/transactions');
+
 app.use(express.json())
 app.use(cors())
+
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 app.use('/routes/', diary);
